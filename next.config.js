6cb9e0caca7afaec885462ebe06060ad1d8be852
Johannes Wittmann <johannes.wittmann1@gmx.de>
2022-02-03 11:37:44 +0100

    NEXTJSCONFIG

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+  env: {
+    APIKEY:"AIzaSyC7v2yG2jySUuFKJSs5e5GXPPQ3qU8iZt4",
+    AUTHDOMAIN:"surveyuib.firebaseapp.com",
+    PROJECTID:"surveyuib",
+    STORAGEBUCKET:"surveyuib.appspot.com",
+    SENDER:"415774266492",
+    APPID:"1:415774266492:web:0686d63b4e002f40e7a5f9",
+    MEASUREMENT:"G-FR8445J652",
+  },
 }
